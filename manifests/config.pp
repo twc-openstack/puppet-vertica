4730ed2c186289c8b07476363f49b2cd2e5f2dab
bklei <bradley.klein@twcable.com>
2015-05-07 15:51:54 -0600

    Disable swap and read-ahead until better understood

--- manifests/config.pp
+++ manifests/config.pp
@@ -46,36 +46,36 @@ class vertica::config(
     line => "export TZ=${time_zone}",
   }
 
-  exec { "Ensure read ahead is set to ${ra_bytes} bytes":
-    command => "blockdev --setra ${ra_bytes} ${file_system}",
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
-    user    => 'root',
-    group   => 'root',
-  }
-
-  exec { "Ensure read ahead command is in /etc/rc.local":
-    command => "sed -i 's#^exit 0#blockdev --setra ${ra_bytes} ${file_system}\\n\0#' /etc/rc.local",
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
-    user    => 'root',
-    group   => 'root',
-    onlyif  => "grep -q 'blockdev --setra ${ra_bytes} ${file_system}' /etc/rc.local; test $? -ne 0",
-  }
-
-  exec { "Ensure ${swap_file} exists":
-    command => "dd if=/dev/zero of=${swap_file} bs=1024 count=2097152 && mkswap ${swap_file} && swapon ${swap_file}",
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
-    user    => 'root',
-    group   => 'root',
-    onlyif  => "test -f ${swap_file}; test $? -ne 0",
-  }
-
-  exec { "Ensure ${swap_file} is in fstab":
-    command => "echo '${swap_file} none swap sw 0 0' >> /etc/fstab",
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
-    user    => 'root',
-    group   => 'root',
-    onlyif  => "grep -q '${swap_file}' /etc/fstab; test $? -ne 0",
-  }
+#  exec { "Ensure read ahead is set to ${ra_bytes} bytes":
+#    command => "blockdev --setra ${ra_bytes} ${file_system}",
+#    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
+#    user    => 'root',
+#    group   => 'root',
+#  }
+#
+#  exec { "Ensure read ahead command is in /etc/rc.local":
+#    command => "sed -i 's#^exit 0#blockdev --setra ${ra_bytes} ${file_system}\\n\0#' /etc/rc.local",
+#    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
+#    user    => 'root',
+#    group   => 'root',
+#    onlyif  => "grep -q 'blockdev --setra ${ra_bytes} ${file_system}' /etc/rc.local; test $? -ne 0",
+#  }
+#
+#  exec { "Ensure ${swap_file} exists":
+#    command => "dd if=/dev/zero of=${swap_file} bs=1024 count=2097152 && mkswap ${swap_file} && swapon ${swap_file}",
+#    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
+#    user    => 'root',
+#    group   => 'root',
+#    onlyif  => "test -f ${swap_file}; test $? -ne 0",
+#  }
+#
+#  exec { "Ensure ${swap_file} is in fstab":
+#    command => "echo '${swap_file} none swap sw 0 0' >> /etc/fstab",
+#    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/tmp',
+#    user    => 'root',
+#    group   => 'root',
+#    onlyif  => "grep -q '${swap_file}' /etc/fstab; test $? -ne 0",
+#  }
 
   file { '/opt/vertica/config/d5415f948449e9d4c421b568f2411140.dat':
     ensure  => file,
